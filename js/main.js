6fd37ef69052700f3c93a0ba528d8b01741adc5f
Abdushakur <abdushakur97@mail.ru>
2022-04-11 09:25:57 +0500

    Lesson one

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-main.js
+let socket = new WebSocket("wss://javascript.info/article/websocket/chat/ws");
+
+// отправка сообщения из формы
+document.forms.publish.onsubmit = function() {
+  let outgoingMessage = this.message.value;
+
+  socket.send(outgoingMessage);
+  return false;
+};
+
+// получение сообщения - отобразить данные в div#messages
+socket.onmessage = function(event) {
+  let message = event.data;
+
+  let messageElem = document.createElement('div');
+  messageElem.textContent = message;
+  document.getElementById('messages').prepend(messageElem);
+}
